b30bc6ccefc28cd34dfb17b4bef574041f7c8801
mariomadproductions <mariomadproductions@github.com>
2022-01-14 15:49:18 +0000

    Started adding functions for directory management

--- yt-dlp-archive.py
+++ yt-dlp-archive.py
@@ -3,6 +3,20 @@ import subprocess
 import argparse
 import datetime
 import json
+import tempfile
+from pathlib import Path
+
+def mk_video_dir(base_dir):
+    return tempfile.mkdtemp(dir=base_dir)
+
+def get_json_name_noext(video_dir):
+    SUFFIX = '.info.json'
+    for path in video_dir.glob(f'*{SUFFIX}'):
+        return path.name[:-len(SUFFIX)]
+
+def ren_video_dir(video_dir):
+    new_name = get_json_name_noext(video_dir)
+
 
 def get_date():
     #from https://stackoverflow.com/a/28147286/2352336
@@ -16,32 +30,35 @@ def validate_yt_dlp_ver(yt_dlp_ver):
         datetime.datetime.strptime(yt_dlp_ver, '%Y.%m.%d')
     except ValueError:
         raise ValueError("Incorrect data format, should be YYYY-MM-DD")
-        
+
 def get_yt_dlp_ver():
-    result = subprocess.run(['yt-dlp', '--version'],stdout=subprocess.PIPE)
+    result = subprocess.run(['yt-dlp', '--version'],
+                             stdout=subprocess.PIPE)
     yt_dlp_ver = result.stdout.decode('utf8').strip()
     validate_yt_dlp_ver(yt_dlp_ver)
     return yt_dlp_ver
 
-def dl_vid(yt_dlp_options,url):
-    subprocess.run(['yt-dlp'] + yt_dlp_options + [url])
+def dl_vid(yt_dlp_options,url,video_dir):
+    subprocess.run(['yt-dlp'] + yt_dlp_options + [url],
+    cwd=video_dir)
 
-def write_info_file(date,yt_dlp_ver,yt_dlp_options):
+def write_info_file(date,yt_dlp_ver,yt_dlp_options,
+                    video_dir):
     INFO_FILE_NAME = 'info.json'
-    
+    INFO_FILE_PATH = video_dir.joinpath(INFO_FILE_NAME)
     info_dict = {}
     info_dict['date_downloaded_on'] = date
     info_dict['yt_dlp_version'] = yt_dlp_ver
     info_dict['yt_dlp_options'] = yt_dlp_options
-    
-    with open(INFO_FILE_NAME, 'w') as info_file:
+
+    with open(INFO_FILE_PATH, 'w') as info_file:
         json.dump(info_dict,info_file, indent=4)
 
 def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('url')
     args = arg_parser.parse_args()
-    
+
     YT_DLP_OPTIONS = ['--write-info-json',
                       '--write-thumbnail',
                       '--write-annotations',
@@ -51,9 +68,12 @@ def main():
     DATE = get_date()
     YT_DLP_VER = get_yt_dlp_ver()
     URL = args.url
-    
-    dl_vid(YT_DLP_OPTIONS,URL)
-    write_info_file(DATE,YT_DLP_VER,YT_DLP_OPTIONS)
+
+    video_dir = mk_video_dir('.')
+    dl_vid(YT_DLP_OPTIONS,URL,video_dir)
+    write_info_file(DATE,YT_DLP_VER,YT_DLP_OPTIONS,
+                    video_dir)
+    ren_video_dir(video_dir)
 
 if __name__ == '__main__':
     main()
